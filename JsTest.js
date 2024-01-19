f92e78abdb21a46ce8e7f33f6d9f3847fa98e4d9
Moh-hamdaoui <157033392+Moh-hamdaoui@users.noreply.github.com>
2024-01-19 10:56:35 +0100

    Premier commit

--- JsTest.js
+++ JsTest.js
@@ -1,3 +1,9 @@
-let a, b;
-a=5;
-b=10;
+let a = 5, b = 10;
+
+let c = a + b;
+
+console.log("la somme est égale à", c);
+
+if (a < b) {
+    console.log("a est inférieur à b");
+}
---
+++ ParcourirTab.js
@@ -0,0 +1,14 @@
+
+
+let Etudiants = [];
+
+Etudiants.push("Etudiant 1");
+Etudiants.push("Etudiant 2");
+Etudiants.push("Etudiant 3");
+Etudiants.push("Etudiant 4");
+Etudiants.push("Etudiant 5");
+Etudiants.push("Etudiant 6");
+
+for (let i = 0 ; i < Etudiants.length ; i++){
+    console.log("l'element ", i , "de tableau est : ", Etudiants[i]);
+}
